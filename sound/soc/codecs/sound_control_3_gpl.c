b34d7b8fd8efecd5c708854675957eeae8f9c595
Paul Reioux <reioux@gmail.com>
2014-04-10 23:41:55 -0700

    Sound Control 3.x: adaptation for HTC One (M8)
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- sound/soc/codecs/sound_control_3_gpl.c
+++ sound/soc/codecs/sound_control_3_gpl.c
@@ -175,7 +175,7 @@ static ssize_t cam_mic_gain_show(struct kobject *kobj,
 {
         return sprintf(buf, "%u",
 		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_TX6_VOL_CTL_GAIN));
+			TAIKO_A_CDC_TX4_VOL_CTL_GAIN));
 
 }
 
@@ -188,7 +188,7 @@ static ssize_t cam_mic_gain_store(struct kobject *kobj,
 
 	if (calc_checksum(lval, 0, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_TX6_VOL_CTL_GAIN, lval);
+			TAIKO_A_CDC_TX4_VOL_CTL_GAIN, lval);
 	}
 	return count;
 }
@@ -198,7 +198,7 @@ static ssize_t mic_gain_show(struct kobject *kobj,
 {
 	return sprintf(buf, "%u",
 		taiko_read(fauxsound_codec_ptr,
-			TAIKO_A_CDC_TX7_VOL_CTL_GAIN));
+			TAIKO_A_CDC_TX3_VOL_CTL_GAIN));
 }
 
 static ssize_t mic_gain_store(struct kobject *kobj,
@@ -210,7 +210,7 @@ static ssize_t mic_gain_store(struct kobject *kobj,
 
 	if (calc_checksum(lval, 0, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_TX7_VOL_CTL_GAIN, lval);
+			TAIKO_A_CDC_TX3_VOL_CTL_GAIN, lval);
 	}
 	return count;
 
@@ -248,9 +248,9 @@ static ssize_t headphone_gain_show(struct kobject *kobj,
 {
 	return sprintf(buf, "%u %u",
 			taiko_read(fauxsound_codec_ptr,
-				TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL),
+				TAIKO_A_CDC_RX3_VOL_CTL_B2_CTL),
 			taiko_read(fauxsound_codec_ptr,
-				TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL));
+				TAIKO_A_CDC_RX5_VOL_CTL_B2_CTL));
 }
 
 static ssize_t headphone_gain_store(struct kobject *kobj,
@@ -262,9 +262,9 @@ static ssize_t headphone_gain_store(struct kobject *kobj,
 
 	if (calc_checksum(lval, rval, chksum)) {
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX1_VOL_CTL_B2_CTL, lval);
+			TAIKO_A_CDC_RX3_VOL_CTL_B2_CTL, lval);
 		taiko_write(fauxsound_codec_ptr,
-			TAIKO_A_CDC_RX2_VOL_CTL_B2_CTL, rval);
+			TAIKO_A_CDC_RX5_VOL_CTL_B2_CTL, rval);
 	}
 	return count;
 }
